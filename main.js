2a58636b9c0a2bfe8a0f46c062f1205757ba105a
Vladilyse <92036521+Vladilyse@users.noreply.github.com>
2022-02-05 14:11:43 +0200

    Tasks_for_JS

---
+++ main.js
@@ -0,0 +1,32 @@
+// For1
+// let y;
+// for(let x = 1; x <=10; x++) {
+//     y=x**2;
+//     console.log("Значение функции y=x^2: "+ y);
+//     console.log("---------------")
+// }
+
+// For2
+// let n = +prompt("Введите конечное значение числа n");
+// let k = 0;
+// let s = 0;
+// for(s ;k <= n; k++) {
+//     s+=k;
+// }
+// console.log("S = " + s)
+
+// For3
+// let a = 10000;
+//     years = 0;
+// for(let p = 1.1; years < 5; years++){
+//     a*=p;
+// }
+// console.log("Через 5 лет кентик поднимет " + a + " баксов")
+
+// For4
+// for(let a = 2; a <= 10; a++){
+//     for(let b = 1; b <= 10; b++){
+//         console.log(`${a} * ${b} = ${a*b}`)
+//     }
+//     console.log('________________')
+// }
